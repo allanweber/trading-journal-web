61bbba7e1649a8ac71e226f6ed4199c02be4630b
Allan <a.cassianoweber@gmail.com>
2023-11-24 19:20:09 +0100

    get journals test

--- src/pages/app/Trading.tsx
+++ src/pages/app/Trading.tsx
@@ -1,3 +1,42 @@
+import { useKindeAuth } from '@kinde-oss/kinde-auth-react';
+import { useEffect, useState } from 'react';
+
 export const Trading = () => {
-  return <div>Trading Journal Main Page</div>;
+  const [journals, setJournals] = useState([]);
+
+  const { getToken } = useKindeAuth();
+
+  const fetchBooks = async () => {
+    try {
+      const accessToken = await getToken();
+      const res = await fetch('http://localhost:8080/api', {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      const data = await res.json();
+      setJournals(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  console.log('journals', journals);
+
+  return (
+    <>
+      <div>Trading Journal Main Page</div>
+      <ul>
+        {journals?.map((journal: any) => (
+          <li key={journal.name}>
+            {journal.name} {journal.description}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
