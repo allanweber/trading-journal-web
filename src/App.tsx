a810515a9db90b518dc8ed9e03c5737111451386
Allan <a.cassianoweber@gmail.com>
2023-11-25 15:01:13 +0100

    vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { inject } from '@vercel/analytics';
 import { AuthContext } from 'contexts/AuthContext';
 import { router } from 'pages/Routes';
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
+
+inject();
 
 function App() {
   return (
