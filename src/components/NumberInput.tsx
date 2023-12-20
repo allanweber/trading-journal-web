050d953d255ea9adbd734da75c9cb8e268e685ef
Allan <a.cassianoweber@gmail.com>
2023-12-20 18:54:57 +0100

    fix NumberInput

--- src/components/NumberInput.tsx
+++ src/components/NumberInput.tsx
@@ -1,9 +1,9 @@
-import { LegacyRef, forwardRef, useEffect, useState } from 'react';
+import { LegacyRef, useState } from 'react';
 import { useIMask } from 'react-imask';
 
 type Props = {
   onChange: (value: number | undefined) => void;
-  value: number;
+  value: number | null | undefined;
   currency?: string;
   placeholder?: string;
   scale?: number;
@@ -15,13 +15,15 @@ const defaultOpts: Partial<Props> = {
   scale: 2,
 };
 
-export const NumberInput = forwardRef(function NumberInput(
-  props: Props,
-  compRef
-) {
-  const { onChange, value, currency, placeholder, scale, ...rest } = props;
-
-  const [opts] = useState({
+export const NumberInput = ({
+  onChange,
+  value,
+  currency,
+  placeholder,
+  scale,
+  ...rest
+}: Props) => {
+  const [opts, setOps] = useState({
     mask: currency ? `${currency} num` : 'num',
     lazy: false,
     blocks: {
@@ -32,39 +34,51 @@ export const NumberInput = forwardRef(function NumberInput(
         mapToRadix: ['.'],
         scale: scale,
         padFractionalZeros: true,
+        normalizeZeros: true,
       },
     },
   });
 
-  const { ref, unmaskedValue, setUnmaskedValue } = useIMask(opts);
+  // useEffect(() => {
+  //   setOps({
+  //     mask: currency ? `${currency} num` : 'num',
+  //     lazy: false,
+  //     blocks: {
+  //       num: {
+  //         mask: Number,
+  //         radix: ',',
+  //         thousandsSeparator: '.',
+  //         mapToRadix: ['.', ','],
+  //         scale: scale,
+  //         padFractionalZeros: false,
+  //         normalizeZeros: true,
+  //         max: Number.MAX_VALUE,
+  //         min: 0,
+  //       },
+  //     },
+  //   });
+  // }, [currency, scale]);
 
-  useEffect(() => {
-    setUnmaskedValue(value ? value.toString() : '');
-  }, [setUnmaskedValue, value]);
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const parsedValue = parseFloat(unmaskedValue);
-    if (isNaN(parsedValue)) {
-      onChange(undefined);
-    } else {
-      onChange(parsedValue);
-    }
-  };
+  const { ref } = useIMask(opts, {
+    onAccept: (val) => {
+      const num = parseFloat(val.replace('.', '').replace(',', '.'));
+      onChange(num);
+    },
+  });
 
   return (
     <div className="pt-2">
       <input
-        defaultValue={value}
+        defaultValue={value || ''}
         placeholder={placeholder || '0'}
         type={'text'}
         className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
         ref={ref as LegacyRef<HTMLInputElement>}
-        onChange={handleChange}
         onFocus={(e) => e.target.select()}
         {...rest}
       />
     </div>
   );
-});
+};
 
 NumberInput.defaultProps = defaultOpts;
