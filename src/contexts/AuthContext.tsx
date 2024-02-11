15c04f853959515ca56fe997b44ba7f403144b8f
Allan <a.cassianoweber@gmail.com>
2024-02-11 11:26:15 +0100

    Persisting authentication state on page refresh or new tab

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,9 +1,10 @@
-import { KindeProvider } from '@kinde-oss/kinde-auth-react';
-import { ReactNode } from 'react';
+import { KindeProvider } from "@kinde-oss/kinde-auth-react";
+import { ReactNode } from "react";
 
 export const AuthContext = ({ children }: { children: ReactNode }) => {
   return (
     <KindeProvider
+      isDangerouslyUseLocalStorage={true}
       clientId={process.env.REACT_APP_KINDE_CLIENT_ID!}
       domain={process.env.REACT_APP_KINDE_DOMAIN!}
       redirectUri={process.env.REACT_APP_KINDE_REDIRECT_URI!}
